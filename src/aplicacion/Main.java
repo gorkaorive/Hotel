b5314def468db7165cd0e7fad94300146945f76f
gorkaorive <g_orive@fpzornotza.com>
2021-02-11 13:36:01 +0100

    Commit 1

---
+++ src/aplicacion/Main.java
@@ -0,0 +1,15 @@
+package aplicacion;
+
+import java.sql.Connection;
+
+public class Main {
+
+	public final static int GESTIONAR_USUARIOS_HOTEL = 1;
+	public final static int GESTIONAR_HOTELES_Y_HABITACIONES = 2;
+	public final static int RESERVAS_DEL_HOTEL = 3;
+	
+	
+
+	public static void main(String[] args) {
+
+
